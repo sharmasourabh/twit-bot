90c0a04711f5e638e49e87e2eada262a245d90fd
Sourabh Sharma <to.sourabh@gmail.com>
2017-02-04 13:25:02 +0000

    add environment variables in config.js

--- src/config.js
+++ src/config.js
@@ -7,17 +7,12 @@
 
 // Uncomment for when deploying to Heroku
 
-// module.exports = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// };
+ module.exports = {
+   consumer_key: process.env.CONSUMER_KEY,
+   consumer_secret: process.env.CONSUMER_SECRET,
+   access_token: process.env.ACCESS_TOKEN,
+   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+ };
 
-module.exports = {
-  consumer_key: 'msUcC0VmqZZHGNV89nhF6vanX',
-  consumer_secret: 'bknoiEHJG92wFASWkIqfnDpvMFPHvzC5YFdY2KgtVmtZpuNRji',
-  access_token: '96699649-eukN8QT2Y24HZtZrR8DY0wImNjfBQeFgymB43m0S6',
-  access_token_secret: 'tRfG00bdqlT8zjC0BBsG1rPc8PGrX0WjQDoHJd9NEfcKl'
-}
+
 
